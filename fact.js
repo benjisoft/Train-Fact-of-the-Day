551116da99b4627c996bb411e606373f8dcfa3ba
Ben Lewis <ben.lewis@visimedia.co.uk>
2022-10-20 22:55:49 +0100

    Updated train fact

--- fact.js
+++ fact.js
@@ -5,8 +5,9 @@ Date.prototype.addDays = function(days) {
 }
 
 function setFact() {
-	document.getElementById("fact").innerHTML = "Trains typically cannot go up hills";
-	document.getElementById("citation").innerHTML = "<a target='_blank' href='https://www.youtube.com/watch?v=KbUsKWbOqUU'>https://www.youtube.com/watch?v=KbUsKWbOqUU</a>";
+	document.getElementById("fact").innerHTML = "Train tracks are split into sections called blocks. If two trains are in one block, this is called a crash. ";
+	//document.getElementById("citation").innerHTML = "<a target='_blank' href='https://www.youtube.com/watch?v=KbUsKWbOqUU'>https://www.youtube.com/watch?v=KbUsKWbOqUU</a>";
+	document.getElementById("citation").innerHTML = "";
 	const today = new Date();
 	const options = { year: 'numeric', month: 'long', day: 'numeric' };
 	document.getElementById("lastUpdated").innerText = today.toLocaleDateString(undefined, options) + " 7:00pm BST";
